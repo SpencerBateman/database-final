3a6f5c13a592abf780c884c122d886a15d69e4f2
Spencer Bateman <bateman.s@husky.neu.edu>
2017-11-12 01:06:57 -0500

    started to flesh out the user schema, added list of likes and list of matches

--- server/model/user/user.schema.server.js
+++ server/model/user/user.schema.server.js
@@ -3,8 +3,10 @@ var UserSchema = mongoose.Schema ({
   username: String,
   password: String,
   firstName: String,
-  lastName: String,
-  age: String
+  age: String,
+  bio: String,
+  listOfLiked: [{type: mongoose.Schema.Types.ObjectId, ref: 'UserModel'}],
+  listOfMatches: [{type: mongoose.Schema.Types.ObjectId, ref: 'UserModel'}]
 }, {collection: 'user'});
 
 module.exports = UserSchema;
