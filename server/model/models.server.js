4082af8bf5b65ba21e2ccee14dc1753245fd350e
Spencer Bateman <bateman.s@husky.neu.edu>
2017-12-08 22:32:24 -0500

    hopefully the final commit

--- server/model/models.server.js
+++ server/model/models.server.js
@@ -1,3 +1,3 @@
 var mongoose = require('mongoose');
-var db = mongoose.connect('mongodb://127.0.0.1:27017/hotmother', { useMongoClient: true});
+var db = mongoose.connect('mongodb://127.0.0.1:27017/daterbase', { useMongoClient: true});
 module.exports = db;
